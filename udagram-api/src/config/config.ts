5e08dd2a0d456d8876256dbc40df01335ebaf128
Mo'men Maged <momen99223@gmail.com>
2022-06-30 21:13:44 +0200

    default profile

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -10,7 +10,7 @@ export const config = {
   host: process.env.POSTGRES_HOST,
   dialect: "postgres",
   aws_region: process.env.AWS_REGION,
-  aws_profile: process.env.AWS_DEFAULT_PROFILE,
+  aws_default_profile: process.env.AWS_DEFAULT_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
   jwt: {
